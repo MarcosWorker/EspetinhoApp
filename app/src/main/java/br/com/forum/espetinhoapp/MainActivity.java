77982bdf453db9d6102d63f6c095b08e09019cea
MarcosWorker <marcos_worker@outlook.com>
2017-09-28 22:15:22 -0300

    Initial commit

---
+++ app/src/main/java/br/com/forum/espetinhoapp/MainActivity.java
@@ -0,0 +1,45 @@
+package br.com.forum.espetinhoapp;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_lista:
+                    mTextMessage.setText("lista");
+                    return true;
+                case R.id.navigation_pedido:
+                    mTextMessage.setText("faça um pedido");
+                    return true;
+                case R.id.navigation_comentarios:
+                    mTextMessage.setText("comentarios");
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
