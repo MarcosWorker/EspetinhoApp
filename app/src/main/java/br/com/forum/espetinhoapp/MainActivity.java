bef67480727be878fdcd0d4eb2fc7b7dd822aa6e
MarcosWorker <marcos_worker@outlook.com>
2017-09-29 18:29:01 -0300

    estrutura das telas

--- app/src/main/java/br/com/forum/espetinhoapp/MainActivity.java
+++ app/src/main/java/br/com/forum/espetinhoapp/MainActivity.java
@@ -4,12 +4,14 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    private Toolbar myToolbar = null;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -17,14 +19,17 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_lista:
-                    mTextMessage.setText("lista");
+                case R.id.navigation_cardapio:
+                    myToolbar.setTitle("Cardápio");
                     return true;
                 case R.id.navigation_pedido:
-                    mTextMessage.setText("faça um pedido");
+                    myToolbar.setTitle("Pedidos");
                     return true;
                 case R.id.navigation_comentarios:
-                    mTextMessage.setText("comentarios");
+                    myToolbar.setTitle("Comentarios");
+                    return true;
+                case R.id.navigation_local:
+                    myToolbar.setTitle("Localização");
                     return true;
             }
             return false;
@@ -37,9 +42,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+        myToolbar = (Toolbar) findViewById(R.id.toolbar_main);
+        myToolbar.setTitle("Cardápio");
+        myToolbar.setTitleTextColor(getResources().getColor(R.color.branco));
+        myToolbar.setNavigationIcon(R.mipmap.ic_launcher);
+        setSupportActionBar(myToolbar);
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.menu_config:
+                Toast.makeText(this, "Em construção", Toast.LENGTH_SHORT).show();
+                myToolbar.setTitle("Configurações");
+                break;
+        }
+        return true;
+    }
+
 
 }
